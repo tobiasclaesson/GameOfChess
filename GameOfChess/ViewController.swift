a9cb34798c89c7653e5cb557e6b687f30f38c593
Tobias Claesson <tobbe.classon@hotmail.se>
2020-01-17 15:14:04 +0100

    Initial Commit

---
+++ GameOfChess/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameOfChess
+//
+//  Created by Tobias Classon on 2020-01-17.
+//  Copyright © 2020 Tobias Classon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
